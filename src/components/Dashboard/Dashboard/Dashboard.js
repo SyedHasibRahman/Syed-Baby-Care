347734267f60420262e16f093e60326e8c925e5f
SyedHasibRahman <syedhasib01@gmail.com>
2021-11-13 22:44:43 +0600

    Change Footer

--- src/components/Dashboard/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard/Dashboard.js
@@ -82,14 +82,16 @@ function Dashboard(props) {
             }
             <br />
 
-            { user?.email ?
-                <button onClick={ logOut } className="btn btn-light">LogOut </button> :
-                <Link className="nav-link active" aria-current="page" to="/Login">Login</Link>
-            }
-            <span className="text-danger">
-                <br />
-                <small>{ user?.displayName }</small>
-            </span>
+            <Link>
+                { user?.email ?
+                    <button onClick={ logOut } className="btn btn-light">LogOut </button> :
+                    <Link className="nav-link active" aria-current="page" to="/Login">Login</Link>
+                }
+                <span className="text-danger">
+                    <br />
+                    <small>{ user?.displayName }</small>
+                </span>
+            </Link>
         </div >
     );
 
